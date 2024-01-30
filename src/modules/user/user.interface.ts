d74543645d685aad90f013670acb3a4a46415261
DucChinh <ducchinhcp882002@gmail.com>
2024-01-30 15:22:31 +0700

    fix validate

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -6,20 +6,26 @@ import Joi from 'joi';
 import { CommonListQuery } from '../../common/interfaces';
 import { IsEmail, MinLength } from 'class-validator';
 
+const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth() + 1;
+const currentDay = new Date().getDate();
 export class CreateUserDto {
     @ApiProperty({
         type: String,
         maxLength: INPUT_TEXT_MAX_LENGTH,
         default: 'User name',
     })
-    @JoiValidate(Joi.string().trim().max(INPUT_TEXT_MAX_LENGTH).required())
+    @JoiValidate(Joi.string().trim().min(10).max(INPUT_TEXT_MAX_LENGTH).required())
     name: string;
 
     @ApiProperty({
         type: String,
         default: '123123'
     })
-    @JoiValidate(Joi.string().trim().min(8).max(INPUT_TEXT_MAX_LENGTH)
+    @JoiValidate(Joi.string()
+        .regex(/^\S*$/)
+        .regex(/^[^\p{So}]+$/u)
+        .trim().min(8).max(INPUT_TEXT_MAX_LENGTH)
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])'))
         .message('Mật khẩu phải chứa ít nhất một chữ cái thường, một chữ cái in hoa và một số.')
         .required())
@@ -38,14 +44,17 @@ export class CreateUserDto {
         type: Date,
         default: '2002-08-08'
     })
-    @JoiValidate(Joi.date().required())
+    @JoiValidate(Joi.date()
+        .min(new Date('1900-01-01'))
+        .max(new Date(`${currentYear}-${currentMonth}-${currentDay}`))
+        .required())
     birthday: Date;
 
     @ApiProperty({
         type: Number,
         default: 0
     })
-    @JoiValidate(Joi.number().required())
+    @JoiValidate(Joi.number().positive().min(9).required())
     phone: number;
 
     @ApiProperty({
