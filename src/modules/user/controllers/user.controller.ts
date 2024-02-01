d48df1779441df4172f682bef0093a77789e603d
DucChinh <ducchinhcp882002@gmail.com>
2024-02-01 14:06:45 +0700

    .

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -38,8 +38,8 @@ import { JoiValidationPipe } from '../../../common/pipe/joi.validation.pipe';
 import { UserService } from '../services/user.service';
 import { Roles } from '../../../roles/roles.decorator';
 import { Role } from '../../../roles/role.enum';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { CloudinaryService } from '@/modules/cloudinary/cloudinary.service';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { CloudinaryService } from '@/modules/cloudinary/cloudinary.service';
 
 
 
@@ -47,7 +47,7 @@ import { CloudinaryService } from '@/modules/cloudinary/cloudinary.service';
 @Controller('user')
 export class UserController extends BaseController {
     constructor(private readonly userService: UserService,
-        private readonly cloudinaryService: CloudinaryService
+        // private readonly cloudinaryService: CloudinaryService
     ) {
         super();
     }
@@ -56,7 +56,7 @@ export class UserController extends BaseController {
     @ApiResponseError([SwaggerApiType.CREATE])
     @ApiResponseSuccess(createUserSuccessResponseExample)
     @ApiBody({ type: CreateUserDto })
-    @UseInterceptors(FileInterceptor('avatar'))
+    // @UseInterceptors(FileInterceptor('avatar'))
     @Post()
     async createUser(
         @Body(new TrimBodyPipe(), new JoiValidationPipe())
@@ -77,9 +77,9 @@ export class UserController extends BaseController {
 
                 return false;
             }
-            if (avatar != null) {
-                dto.avatar = await this.cloudinaryService.uploadAvatar(avatar);
-            }
+            // if (avatar != null) {
+            //     dto.avatar = await this.cloudinaryService.uploadAvatar(avatar);
+            // }
             const result = await this.userService.createUser(dto);
             return new SuccessResponse(result);
 
